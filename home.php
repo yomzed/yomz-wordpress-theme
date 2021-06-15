8a62104211c8bcc1ae40b4992e1be0a908b5776f
Yomz <martinezyoan@gmail.com>
2021-06-15 16:46:22 +0200

    Posts/Single

---
+++ home.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+    <main class="list-post">
+	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+
+        <article class="list-post-article">
+            <?php the_post_thumbnail(); ?>
+            <div>
+                <h3><?php the_category(); ?></h3>
+                <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time( get_option( 'date_format' ) ); ?></time>
+                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            </div>
+        </article>
+
+	<?php endwhile; endif; ?>
+    </main>
+<?php get_footer(); ?>
---
+++ single.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+
+        <main>
+            <article class="main-article">
+                <header>
+                    <h2><?php the_category(); ?></h2>
+                    <h1><?php the_title(); ?></h1>
+                    <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time( get_option( 'date_format' ) ); ?></time>
+                </header>
+                <?php the_content(); ?>
+            </article>
+        </main>
+        <aside></aside>
+
+	<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
