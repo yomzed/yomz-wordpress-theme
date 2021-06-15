8b39ff3151986ac716ad5cf307b80d9e4d6c0d07
Yomz <martinezyoan@gmail.com>
2021-06-15 16:50:35 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -5,3 +5,20 @@ add_theme_support( 'post-thumbnails' );
 
 // Ajouter automatiquement le titre du site dans l'en-tête du site
 add_theme_support( 'title-tag' );
+
+// function yomzpress_remove_menu_pages() {
+// 	remove_menu_page( 'tools.php' );
+//     remove_menu_page( 'edit-comments.php' );
+// }
+// add_action( 'admin_menu', 'yomzpress_remove_menu_pages' );
+
+function yomzpress_register_assets() { 
+    // Déclarer style.css à la racine du thème
+    wp_enqueue_style( 
+        'yomzpress',
+        get_stylesheet_uri(), 
+        array(), 
+        '1.0'
+    );
+}
+add_action( 'wp_enqueue_scripts', 'yomzpress_register_assets' );
