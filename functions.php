25edd9df655de8ebd63e1928be588623f037bbe9
Yomz <martinezyoan@gmail.com>
2021-06-16 09:32:33 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -33,6 +33,8 @@ remove_action('wp_head', 'wp_generator');
 remove_action('wp_head', 'start_post_rel_link');
 remove_action('wp_head', 'index_rel_link');
 remove_action('wp_head', 'adjacent_posts_rel_link');
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
 
 // Retrait de la barre Admin
 add_filter( 'show_admin_bar', '__return_false' );
