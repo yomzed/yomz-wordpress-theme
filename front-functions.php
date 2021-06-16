ad465ce3cb17d120b044e4feb30b383cea98f2c0
Yomz <martinezyoan@gmail.com>
2021-06-16 18:33:44 +0200

    Schema head

--- front-functions.php
+++ front-functions.php
@@ -31,9 +31,70 @@ function yomzpress_meta_head() {
             $meta['twitter:image'] = get_the_post_thumbnail_url($post, 'medium');
         }
     }
-    echo '<meta name="description" content="' . $description . '">' . "\n";
+    echo '    <meta name="description" content="' . $description . '">' . "\n";
     foreach( $meta as $property => $content ) {
-        echo '<meta property="' . $property . '" content="' . $content . '">' . "\n";
+        echo '    <meta property="' . $property . '" content="' . $content . '">' . "\n";
     }
 }
 add_action( 'wp_head', 'yomzpress_meta_head' );
+
+function yomzpress_schema_head() {
+    $schema = [
+        '@context' => 'https://schema.org',
+        '@type' => 'Blog',
+        'name' => get_bloginfo( 'name' ), 
+        'description' => get_bloginfo( 'description' ),
+        'url' => home_url( '/' ), 
+        'publisher' => [
+            '@type' => 'Organization',
+            'name' => get_bloginfo( 'name' ),
+            'url' => home_url( '/' ),
+            'logo' => [
+                '@type' => 'ImageObject',
+                'url' => get_template_directory_uri() . '/img/favicons/favicon-192x192.png', 
+                'width' => '192',
+                'height' => '192'
+            ]
+        ], 
+        'author' => [
+            '@type' => 'Person',
+            'name' => 'Yoan Martinez',
+            'url' => home_url( '/' )
+        ],
+        'image' => [
+            '@type' => 'ImageObject',
+            'url' => get_template_directory_uri() . '/img/og-image.jpg', 
+            'width' => '1200'
+        ], 
+        'mainEntityOfPage' => [
+            '@type' => 'Blog',
+            '@id' => home_url( '/' )
+        ]
+    ];
+
+    if ( is_singular() ) {
+        $schema['url'] = get_permalink();
+        $schema['description'] = get_the_excerpt();
+        $schema['name'] = wp_strip_all_tags(get_the_title());
+        $schema['headline'] = wp_strip_all_tags(get_the_title());
+
+        if ( is_single() ) {
+            $schema['@type'] = 'Article';
+            $schema['image'] = [
+                '@type' => 'ImageObject',
+                'url' => get_the_post_thumbnail_url($GLOBALS['post'], 'medium'), 
+                'width' => '1200'
+            ]; 
+            $schema['datePublished'] = get_the_time('c');
+            $schema['dateModified'] = get_the_modified_time('c');
+        } else {
+            $schema['@type'] = 'WebPage';
+        }
+    }
+
+    echo '    <script type="application/ld+json">' . "\n";
+    echo '        ' . json_encode($schema) . "\n";
+    echo '    </script>' . "\n";
+}
+
+add_action( 'wp_head', 'yomzpress_schema_head' );
