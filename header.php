324b1cf79f4feee9723540d74fcd51df4ada6d4c
Yomz <martinezyoan@gmail.com>
2021-06-16 13:51:36 +0200

    Update header.php

--- header.php
+++ header.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-    
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> 
     <?php wp_head(); ?>
 </head>
 
