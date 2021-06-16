fd8b809ba81785775151034baa21aac2b8edd0b5
Yomz <martinezyoan@gmail.com>
2021-06-16 17:22:20 +0200

    Update front-functions.php

--- front-functions.php
+++ front-functions.php
@@ -1,5 +1,6 @@
 <?php
 function yomzpress_meta_head() {
+    // Default meta
     $meta = [
         'og:title' => get_bloginfo( 'name' ) . '–' . get_bloginfo( 'description' ),
         'og:site_name' => get_bloginfo( 'name' ),
@@ -12,18 +13,22 @@ function yomzpress_meta_head() {
         'twitter:site' => '@iomzed',
         'twitter:image' => get_template_directory_uri() . '/img/og-image.jpg'
     ];
-    if( is_single() ) {
-        $post = $GLOBALS['post'];
+    // Pages & Posts
+    if( is_singular() ) {
         $meta['og:title'] = wp_strip_all_tags(get_the_title());
         $meta['description'] = get_the_excerpt();
         $meta['og:description'] = get_the_excerpt();
         $meta['og:url'] = get_permalink();
-        $meta['og:type'] = 'article';
-        $meta['og:image'] = get_the_post_thumbnail_url();
-        $meta['twitter:card'] = 'summary_large_image';
-        $meta['twitter:image'] = get_the_post_thumbnail_url();
-        $meta['article:published_time'] = get_the_time('c');
-        $meta['article:modified_time'] = get_the_modified_time('c');
+        
+        // Posts
+        if( is_single() ) {
+            $meta['og:type'] = 'article';
+            $meta['twitter:card'] = 'summary_large_image';
+            $meta['article:published_time'] = get_the_time('c');
+            $meta['article:modified_time'] = get_the_modified_time('c');
+            $meta['og:image'] = get_the_post_thumbnail_url();
+            $meta['twitter:image'] = get_the_post_thumbnail_url();
+        }
     }
     foreach( $meta as $name => $content ) {
         echo '<meta name="' . $name . '" content="' . $content . '">';
