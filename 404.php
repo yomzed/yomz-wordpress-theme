0bc08afb27e482741206d672d5a36d25b419a22d
Yomz <martinezyoan@gmail.com>
2021-06-23 14:38:50 +0200

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,11 +1,11 @@
 <?php get_header(); ?>
 
-    <main>
+    <main class="main-article">
         <h1>Cette page n'existe pas !</h1>
         <p>
             <a href="<?php echo home_url( '/' ); ?>">Retournez à l'accueil</a> ou consultez les derniers articles.
         </p>
-        <?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+        <p><?php the_widget( 'WP_Widget_Recent_Posts' ); ?></p>
     </main>
 
 <?php get_footer(); ?>
