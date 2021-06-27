0618e38f2dd497489285d96e4ed3ca785767945c
Yomz <martinezyoan@gmail.com>
2021-06-27 21:10:49 +0200

    Update comments.php

--- comments.php
+++ comments.php
@@ -4,14 +4,11 @@
         'submit_field' => '<div class="form-submit">%1$s %2$s</div>'
      ) ); ?>
 
-    <? if ( have_comments() ) : ?>
-        <div class="comments-list">
-            <?php 
-                wp_list_comments( array(
-                    'style' => 'div'
-                ) );
-            ?>
-        </div>
-    <?php endif; ?>
-
+    <div class="comments-list">
+        <?php 
+            wp_list_comments( array(
+                'style' => 'div'
+            ) );
+        ?>
+    </div>
 </aside>
