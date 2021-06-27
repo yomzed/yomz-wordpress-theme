01192b8ca4c7deb27a2dd369af44b8ed7b892aac
Yomz <martinezyoan@gmail.com>
2021-06-27 20:39:33 +0200

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,7 +1,7 @@
 <aside class="comments">
     <?php comment_form( array( 
         'comment_notes_before' => '',
-        'comment_field' => '<div class="comment-field><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><br /><textarea id="comment" name="comment" aria-required="true"></textarea></p>'
+        'submit_field' => '<div class="form-submit">%1$s %2$s</div>'
      ) ); ?>
 
     <?php 
