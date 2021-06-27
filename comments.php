79dcbda276cb13161ce91fa9fb75b0c89dfa3fed
Yomz <martinezyoan@gmail.com>
2021-06-27 20:21:54 +0200

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,7 +1,7 @@
 <aside class="comments">
     <h3>Laisser un commentaire</h3>
     <?php comment_form( array( 
-        'comments_notes_before' => ''
+        'comment_notes_before' => ''
      ) ); // Le formulaire d'ajout de commentaire ?>
 
     <?php 
